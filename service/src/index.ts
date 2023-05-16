4f5fd9ff12c72d6e8585b1e41419c0dd2b66e970
nephen <995168694@qq.com>
2023-05-16 22:07:40 +0800

    feat: add jwt

--- service/src/index.ts
+++ service/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import jwt from 'jsonwebtoken'
 import type { RequestProps } from './types'
 import type { ChatMessage } from './chatgpt'
 import { chatConfig, chatReplyProcess, currentModel } from './chatgpt'
@@ -10,7 +11,9 @@ const app = express()
 const router = express.Router()
 
 app.use(express.static('public'))
+
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.all('*', (_, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
@@ -66,14 +69,48 @@ router.post('/session', async (req, res) => {
   }
 })
 
+router.post('/gentoken', async (req, res) => {
+  try {
+    const AUTH_SECRET_KEY = process.env.AUTH_SECRET_KEY
+    const hasAuth = isNotEmptyString(AUTH_SECRET_KEY)
+    const { key, time, userId = 1 } = req.body as { key: string; time: number; userId?: number }
+    if (key !== AUTH_SECRET_KEY)
+      throw new Error('key无效 | Key is invalid')
+
+    if (hasAuth) {
+      // 从请求中获取用户信息或任何其他必要的数据
+      const user = {
+        id: userId,
+      }
+      // 生成带有过期时间的 JWT 令牌
+      const token = jwt.sign(user, AUTH_SECRET_KEY, { expiresIn: `${time}h` })
+      // 将生成的令牌发送回客户端
+      res.json({ token })
+    }
+    else {
+      throw new Error('无需设置密钥 | No need to set token')
+    }
+  }
+  catch (error) {
+    res.send({ status: 'Fail', message: error.message, data: null })
+  }
+})
+
 router.post('/verify', async (req, res) => {
   try {
+    const AUTH_SECRET_KEY = process.env.AUTH_SECRET_KEY
     const { token } = req.body as { token: string }
     if (!token)
       throw new Error('Secret key is empty')
-
-    if (process.env.AUTH_SECRET_KEY !== token)
-      throw new Error('密钥无效 | Secret key is invalid')
+    jwt.verify(token, AUTH_SECRET_KEY, (err, decodedToken) => {
+      if (err) {
+        throw new Error('密钥无效 | Secret key is invalid')
+      }
+      else {
+        const { exp } = decodedToken
+        globalThis.console.log('Expiration time:', new Date(exp * 1000))
+      }
+    })
 
     res.send({ status: 'Success', message: 'Verify successfully', data: null })
   }
--- service/src/middleware/auth.ts
+++ service/src/middleware/auth.ts
@@ -1,13 +1,25 @@
+import jwt from 'jsonwebtoken'
 import { isNotEmptyString } from '../utils/is'
 
 const auth = async (req, res, next) => {
   const AUTH_SECRET_KEY = process.env.AUTH_SECRET_KEY
   if (isNotEmptyString(AUTH_SECRET_KEY)) {
     try {
-      const Authorization = req.header('Authorization')
-      if (!Authorization || Authorization.replace('Bearer ', '').trim() !== AUTH_SECRET_KEY.trim())
-        throw new Error('Error: 无访问权限 | No access rights')
-      next()
+      const authorizationHeader = req.header('Authorization')
+      if (authorizationHeader && authorizationHeader.startsWith('Bearer ')) {
+        const token = authorizationHeader.substring(7)
+        jwt.verify(token, AUTH_SECRET_KEY, (err, user) => {
+          if (err) {
+            globalThis.console.log(err)
+            throw new Error('Error: 无访问权限 | No access rights')
+          }
+          req.user = user
+          next()
+        })
+      }
+      else {
+        throw new Error('Error: 没有提供token | No token provided')
+      }
     }
     catch (error) {
       res.send({ status: 'Unauthorized', message: error.message ?? 'Please authenticate.', data: null })
